da4e8e80404e6a4cd69c4e11064db8f53cb9634e
ICE-GLXY <220166153@mycput.ac.za>
2022-09-27 17:22:17 +0200

    Added StudentManagementMain

---
+++ src/main/java/za/ac/cput/StudentManagementMain.java
@@ -0,0 +1,11 @@
+package za.ac.cput;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementMain {
+    public static void main(String[] args) {
+        SpringApplication.run(StudentManagementMain.class, args);
+    }
+}
