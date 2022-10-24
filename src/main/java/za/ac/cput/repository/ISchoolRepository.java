36c54b75f252451876ebe05d7bb72b665817c66d
Abdul161 <81579420+Abdul161@users.noreply.github.com>
2022-10-24 22:10:25 +0200

    Add files via upload

---
+++ src/main/java/za/ac/cput/repository/ISchoolRepository.java
@@ -0,0 +1,10 @@
+package za.ac.cput.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import za.ac.cput.domain.school;
+
+@Repository
+public interface ISchoolRepository extends JpaRepository<school, String> {
+    public school findByNumber(String id);
+}
