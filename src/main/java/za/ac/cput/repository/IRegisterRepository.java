8b524e38773972552fb16553df73c9deb87ec2db
Tasreeq Adams <81373672+Tasreeq-Adams@users.noreply.github.com>
2022-10-26 12:06:03 +0200

    updated login and register repos
    updated login and register repos

--- src/main/java/za/ac/cput/repository/IRegisterRepository.java
+++ src/main/java/za/ac/cput/repository/IRegisterRepository.java
@@ -6,8 +6,8 @@ package za.ac.cput.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import za.ac.cput.domain.Register;
+import za.ac.cput.domain.register;
 
 @Repository
-public interface IRegisterRepository extends JpaRepository<Register, String> {
+public interface IRegisterRepository extends JpaRepository<register, String> {
 }
