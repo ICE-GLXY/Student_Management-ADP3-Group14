36a11da01baf6f91a5da2f825bd11e7697c69946
Abdul161 <81579420+Abdul161@users.noreply.github.com>
2022-10-26 10:34:48 +0200

    Delete ISchoolRepository.java

--- src/main/java/za/ac/cput/repository/ISchoolRepository.java
+++
@@ -1,9 +0,0 @@
-package za.ac.cput.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import za.ac.cput.domain.school;
-
-@Repository
-public interface ISchoolRepository extends JpaRepository<school, String> {
-}
